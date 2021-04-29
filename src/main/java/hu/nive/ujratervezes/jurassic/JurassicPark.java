92e30f1458f850bfd434a6b92c8b48928d0a52fe
Gabor Vincze <gkvincze@gmail.com>
2021-04-29 12:04:09 +0200

    checkoverpopulation

--- src/main/java/hu/nive/ujratervezes/jurassic/JurassicPark.java
+++ src/main/java/hu/nive/ujratervezes/jurassic/JurassicPark.java
@@ -1,5 +1,7 @@
 package hu.nive.ujratervezes.jurassic;
 
+import java.sql.*;
+
 public class JurassicPark {
 
     private String dbUrl;
@@ -10,6 +12,34 @@ public class JurassicPark {
         this.dbUrl = dbUrl;
         this.dbUser = dbUser;
         this.dbPassword = dbPassword;
+    }
+
+    private Connection getConnection() {
+        Connection connection = null;
+        try {
+            connection = DriverManager.getConnection(dbUrl, dbUser, dbPassword);
+        } catch (SQLException e) {
+            System.err.println("Database not reachable.");
+        }
+        return connection;
+    }
+
+    public String checkOverpopulation() {
+        String overpopulatedSpecies = "";
+        String query = "SELECT breed FROM dinosaur WHERE actual > expected;";
+
+        try (Connection connection = getConnection()) {
+            PreparedStatement statement = connection.prepareStatement(query);
+            ResultSet resultSet = statement.executeQuery();
+
+            if (resultSet.next()) {
+                overpopulatedSpecies = resultSet.getString("breed");
+            }
+
+        } catch (SQLException e) {
+            e.printStackTrace();
+        }
+        return overpopulatedSpecies;
     }
 
 }
